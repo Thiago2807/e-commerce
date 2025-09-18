92bdbab51019ac4152dd65b77382133102e9de8c
Thiago2807 <thiagodasilva410@gmail.com>
2025-09-18 20:35:57 -0300

    feat: Criando arquivo de injeção de dependencia

--- api-gtw/Program.cs
+++ api-gtw/Program.cs
@@ -1,7 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
+builder.Configuration.AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer("Bearer", options =>
+{
+    var secretKey = builder.Configuration.GetSection("JWT:secret-key").Value;
+    var issuer = builder.Configuration.GetSection("JWT:issuer").Value;
+    var audience = builder.Configuration.GetSection("JWT:audience").Value;
+
+    if (string.IsNullOrEmpty(secretKey) || string.IsNullOrEmpty(issuer) || string.IsNullOrEmpty(audience))
+    {
+        throw new Exception("Informa��es para o token JWT inv�lidos, verifique o arquivo de configura��es.");
+    }
+
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.MapInboundClaims = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+    };
+});
+
+builder.Services
+    .AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
 app.UseHttpsRedirection();
+await app.UseOcelot();
 
 app.Run();
---
+++ api-stock/Services/DependencyInjection.cs
@@ -0,0 +1,9 @@
+namespace api_stock.Services;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+    {
+        return services;
+    }
+}
